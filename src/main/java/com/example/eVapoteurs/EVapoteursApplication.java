8e25d876833724afb8e08b8cc9075d6287fc8498
samuel.kebe <samuel.kebe@it-centrex.com>
2024-06-10 14:45:16 +0000

    first commit

---
+++ src/main/java/com/example/eVapoteurs/EVapoteursApplication.java
@@ -0,0 +1,13 @@
+package com.example.eVapoteurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVapoteursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVapoteursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eVapoteurs/EVapoteursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eVapoteurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVapoteursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
