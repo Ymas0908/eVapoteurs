c64c852fdada5360c25cf611169d083bf99688f7
samuel.kebe <samuel.kebe@it-centrex.com>
2024-07-18 17:47:56 +0000

    ""

--- src/main/java/com/example/eVapoteurs/managedbean/ArticlesMB.java
+++ src/main/java/com/example/eVapoteurs/managedbean/ArticlesMB.java
@@ -3,36 +3,28 @@ package com.example.eVapoteurs.managedbean;
 import com.example.eVapoteurs.model.Articles;
 import com.example.eVapoteurs.services.ArticlesServices;
 import jakarta.annotation.PostConstruct;
-import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.List;
 
 @Named
-@ViewScoped
-public class ArticlesMB {
-
-    @Getter
-    @Setter
-    private ArticlesMB profilUtilisateur;
-
+public class ArticlesMB implements Serializable {
 
     @Getter
     @Setter
     private List<Articles> articlesList;
-    @Getter
-    @Setter
+
     @Inject
     private ArticlesServices articlesServices;
 
     @PostConstruct
     public void init() {
-        articlesList = new ArrayList<com.example.eVapoteurs.model.Articles>();
-        articlesList = articlesServices.getAllArticles(10);
-    }
+        Articles Articles = new Articles();
 
+        articlesList = articlesServices.getAllArticles();
+    }
 }
--- src/main/java/com/example/eVapoteurs/services/ArticlesServices.java
+++ src/main/java/com/example/eVapoteurs/services/ArticlesServices.java
@@ -17,18 +17,18 @@ import java.util.List;
 @Service
 public class ArticlesServices {
     private List<Articles> articlesList = new ArrayList<>();
-    public List<Articles> getAllArticles(int size) {
-        articlesList.add(new Articles(1, "Vapoteur 1", "Coca Cola", "coca.jpg", 13000  ));
-        articlesList.add(new Articles(2, "Vapoteur 2", "Coca Cola", "coca.jpg", 1.5));
-        articlesList.add(new Articles(3, "Vapoteur 3", "Coca Cola", "coca.jpg", 1.5));
-        articlesList.add(new Articles(4, "Vapoteur 4", "Coca Cola", "coca.jpg", 1.5));
-        articlesList.add(new Articles(5, "Vapoteur 5", "Coca Cola", "coca.jpg", 1.5));
-        articlesList.add(new Articles(6, "Vapoteur 6", "Coca Cola", "coca.jpg", 1.5));
-        articlesList.add(new Articles(7, "Vapoteur 7", "Coca Cola", "coca.jpg", 1.5));
-        articlesList.add(new Articles(8, "Vapoteur 8", "Coca Cola", "coca.jpg", 1.5));
-        articlesList.add(new Articles(9, "Vapoteur 9", "Coca Cola", "coca.jpg", 1.5));
-        articlesList.add(new Articles(10, "Vapoteur 10", "Coca Cola", "coca.jpg", 1.5));
 
+    public List<Articles> getAllArticles() {
+        articlesList.add(new Articles(1, "Vapoteur 1", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(2, "Vapoteur 2", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(3, "Vapoteur 3", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(4, "Vapoteur 4", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(5, "Vapoteur 5", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(6, "Vapoteur 6", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(7, "Vapoteur 7", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(8, "Vapoteur 8", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(9, "Vapoteur 9", "Coca Cola", "coca.jpg", 13000));
+        articlesList.add(new Articles(10, "Vapoteur 10", "Coca Cola", "coca.jpg", 13000));
         return articlesList;
 
 
